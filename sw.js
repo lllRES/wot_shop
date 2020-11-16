1056551287d6581214180dc390d0ff4d12ea6a8c
IIIRes <bhjSEnkjd3@res.pm>
2020-11-16 15:59:56 +0200

    +

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-47533d460e9a55f57f87.js"
+    "url": "webpack-runtime-686b082a77b899391ef3.js"
   },
   {
     "url": "styles.99f12bbb4968b440c5ab.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6a0e9e4c1a7997f3424a70eb390201b5"
+    "revision": "b8e7ede56dfa7357eb6ba355fd10135b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -57,7 +57,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "7ad43e300d754ff3e282c474f492fa37"
+    "revision": "8a57ee5574bbe60e9d8729b284529733"
   },
   {
     "url": "polyfill-6ed41a637bbcdd69b96c.js"
