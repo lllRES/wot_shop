a93b3abc78d79a732ae861f741f7ea703e6176b0
IIIRes <bhjSEnkjd3@res.pm>
2021-02-04 22:00:46 +0200

    +

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5748e3a472300f9c0866.js"
+    "url": "webpack-runtime-a5eb187b5f7efddfa908.js"
   },
   {
     "url": "framework-c8d3a5345e3f321d7da5.js"
@@ -42,11 +42,11 @@ self.__precacheManifest = [
     "url": "aceecd74-aaa8ba91a287517d9100.js"
   },
   {
-    "url": "app-643e247ff81626ba0aac.js"
+    "url": "app-34da8105e2871fcd3aab.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5fa1fda89db7dce603da26f289f55974"
+    "revision": "ab6bae7e029eab305314868bd9591a5d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
@@ -57,10 +57,73 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "e317cab2662a29a34746b32bf9cf5ad3"
+    "revision": "a071a0fab32b9e6931eb8790a2656083"
   },
   {
     "url": "polyfill-2cb897c2a056d28c9332.js"
+  },
+  {
+    "url": "f75ca9e7a5abc04cc9669fbd65c82944fd2ea495-9ae258410a039f6c9356.js"
+  },
+  {
+    "url": "c0b520c78599ab9d2deccb4c2b61af23d8a1ee4e-d57e403f6b3df0c7cfb5.js"
+  },
+  {
+    "url": "component---src-projects-shop-world-of-tanks-components-pages-index-js-268284eada279a845ad2.js"
+  },
+  {
+    "url": "page-data/index/page-data.json",
+    "revision": "dc40d00df86ead0b3165050b305dd4e1"
+  },
+  {
+    "url": "page-data/sq/d/2353285886.json",
+    "revision": "e7d201268b49ac9df30dfd35b1e8f207"
+  },
+  {
+    "url": "page-data/sq/d/859598031.json",
+    "revision": "607ac0533a81369a8036ab4656e3cd2d"
+  },
+  {
+    "url": "component---src-projects-shop-world-of-tanks-components-pages-rules-js-810d2560525cf24f8aa5.js"
+  },
+  {
+    "url": "page-data/rules/page-data.json",
+    "revision": "d109cc466463f3e3caa100d3a9ec5832"
+  },
+  {
+    "url": "component---src-projects-shop-world-of-tanks-components-pages-faq-js-70d2c019488bd66ebbd9.js"
+  },
+  {
+    "url": "page-data/faq/page-data.json",
+    "revision": "67e87437459988eecee73b72e42bea95"
+  },
+  {
+    "url": "component---src-projects-shop-world-of-tanks-components-pages-404-js-67798541778360dc691d.js"
+  },
+  {
+    "url": "page-data/404/page-data.json",
+    "revision": "36174ea07b827de5b9e3d32786c7f5e2"
+  },
+  {
+    "url": "component---src-projects-shop-world-of-tanks-components-pages-offer-001-js-2414c5f20a5cfc911b08.js"
+  },
+  {
+    "url": "page-data/offer-001/page-data.json",
+    "revision": "ebe3e17ee91237c63e9e310ac714ce63"
+  },
+  {
+    "url": "component---src-projects-shop-world-of-tanks-components-pages-offer-002-js-6c2d7226066e28161dee.js"
+  },
+  {
+    "url": "page-data/offer-002/page-data.json",
+    "revision": "f6141c892810d47f2c95b959cd509f48"
+  },
+  {
+    "url": "component---src-projects-shop-world-of-tanks-components-pages-offer-003-js-e35efa5aab1dc27f229e.js"
+  },
+  {
+    "url": "page-data/offer-003/page-data.json",
+    "revision": "78f98cafa493f0afee6b175c79d4be4b"
   },
   {
     "url": "manifest.webmanifest",
@@ -156,7 +219,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-643e247ff81626ba0aac.js`))) {
+  if (!resources || !(await caches.match(`/app-34da8105e2871fcd3aab.js`))) {
     return await fetch(event.request)
   }
 
